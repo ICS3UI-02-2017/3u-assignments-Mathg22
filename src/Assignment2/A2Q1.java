17b1f095cd4bad6934591af5962a9a4787e9cbe6
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-02-20 11:18:06 -0500

    Assignment2 Written responses complete and Q1 complete

--- src/Assignment2/A2Q1.java
+++ src/Assignment2/A2Q1.java
@@ -4,6 +4,13 @@
  */
 package Assignment2;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Thing;
+import becker.robots.Wall;
+import java.awt.Color;
+
 /**
  *
  * @author mathg8825
@@ -15,7 +22,28 @@ public class A2Q1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+                 //create a city for the robot
+        City ovo = new City();
+        //put a robot in my city
+        RobotSE drake = new RobotSE(ovo, 1, 1, Direction.EAST); 
+        drake.setColor(Color.red); //make drake red
         
+        new Thing(ovo, 1, 2);
+        new Thing(ovo, 1, 3);
+        new Thing(ovo, 1, 4);
+        new Thing(ovo, 1, 5);
+        new Thing(ovo, 1, 6);
+        new Thing(ovo, 1, 7);
+        new Thing(ovo, 1, 8);
+        new Thing(ovo, 1, 9);
+        new Thing(ovo, 1, 10);
+        new Thing(ovo, 1, 11);
+        
+        while(drake.countThingsInBackpack()<7){
+            drake.move();
+            drake.pickThing();
+        }
+        drake.move(4);
         
     }
 }
