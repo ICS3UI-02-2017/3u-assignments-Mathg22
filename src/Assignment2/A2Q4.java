6c0b644778a78ebf51133af11d2cac982072ae6d
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-02-22 11:20:39 -0500

    Assignment2 Written responses complete and Q1, Q2, and Q3 complete

---
+++ src/Assignment2/A2Q4.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment2;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+import java.awt.Color;
+
+/**
+ *
+ * @author mathg8825
+ */
+public class A2Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //create a city for the robot
+        City ovo = new City();
+        //put a robot in my city
+        RobotSE drake = new RobotSE(ovo, 9, 6, Direction.SOUTH);
+        drake.setColor(Color.red); //make drake red
+        //top left pillar
+        new Wall(ovo, 1, 1, Direction.NORTH);
+        new Wall(ovo, 1, 1, Direction.EAST);
+        new Wall(ovo, 1, 1, Direction.SOUTH);
+        new Wall(ovo, 1, 1, Direction.WEST);
+        //middle square
+        new Wall(ovo, 2, 2, Direction.NORTH);
+        new Wall(ovo, 2, 3, Direction.NORTH);
+        new Wall(ovo, 2, 2, Direction.WEST);
+        new Wall(ovo, 3, 2, Direction.WEST);
+        new Wall(ovo, 3, 2, Direction.SOUTH);
+        new Wall(ovo, 3, 3, Direction.SOUTH);
+        new Wall(ovo, 3, 3, Direction.EAST);
+        new Wall(ovo, 2, 3, Direction.EAST);
+        //top right pillar
+        new Wall(ovo, 1, 4, Direction.NORTH);
+        new Wall(ovo, 1, 4, Direction.EAST);
+        new Wall(ovo, 1, 4, Direction.SOUTH);
+        new Wall(ovo, 1, 4, Direction.WEST);
+        //bottom left pillar
+        new Wall(ovo, 4, 1, Direction.NORTH);
+        new Wall(ovo, 4, 1, Direction.EAST);
+        new Wall(ovo, 4, 1, Direction.SOUTH);
+        new Wall(ovo, 4, 1, Direction.WEST);
+        //bottom right pillar
+        new Wall(ovo, 4, 4, Direction.NORTH);
+        new Wall(ovo, 4, 4, Direction.EAST);
+        new Wall(ovo, 4, 4, Direction.SOUTH);
+        new Wall(ovo, 4, 4, Direction.WEST);
+        
+        //castle must remain on the right side
+        
+    }
+}
---
+++ src/Examples/ForLoops.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import java.awt.Color;
+
+/**
+ *
+ * @author mathg8825
+ */
+public class ForLoops {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         //create a city for the robot
+        City ovo = new City();
+        //put a robot in my city
+        RobotSE drake = new RobotSE(ovo, 1, 1, Direction.SOUTH);
+        drake.setColor(Color.red); //make drake red
+        // counted while loop
+        int numberOfMoves = 5;
+        while(numberOfMoves>0){
+            drake.move();
+            numberOfMoves = numberOfMoves -1;
+            
+        }
+    drake.turnAround();
+    
+    //counting the other way
+    numberOfMoves= 0;
+    while(numberOfMoves<5){
+        drake.move();
+        numberOfMoves = numberOfMoves +1;
+    }
+    
+    drake.turnAround();
+    // use a for loop to move
+    for(int count = 0; count <5; count++);
+    drake.move();
+    
+    int x = 10;
+            x = x+5;//adds 5
+            x += 5;//adds 5
+            x -=5; //subtract 5
+            x *=2; //multply by 2
+            x /=2; //divide by 2
+            
+            //modulus or modulo
+            int remainder = 5 % 2; 
+            int quotient = 5 / 2;
+            
+            if(drake.frontIsClear()){
+                if(drake.canPickThing()){
+                    drake.move();}
+                }
+          if(drake.frontIsClear() && drake.canPickThing()){
+          
+          }
+            if(drake.frontIsClear() || drake.canPickThing()){
+          
+          }  
+          
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    
+    }
+   
+}
