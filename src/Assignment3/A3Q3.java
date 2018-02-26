83dabdc4032a2c4575c5c76d95d10438d2d84990
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-02-26 11:19:11 -0500

    Assignment3 Q1 and Q2 complete

--- src/Assignment3/A3Q3.java
+++ src/Assignment3/A3Q3.java
@@ -7,6 +7,7 @@ package Assignment3;
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.RobotSE;
+import becker.robots.Thing;
 import java.awt.Color;
 
 /**
@@ -20,9 +21,24 @@ public class A3Q3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-                City ovo = new City();
-        //put a robot in my city
-        RobotSE drake = new RobotSE(ovo, 2, 2, Direction.EAST);
+        City ovo = new City();
+        //put a robot in my city and give it 20 things
+        RobotSE drake = new RobotSE(ovo, 1, 1, Direction.EAST, 20);
         drake.setColor(Color.red); //make drake red
+        int x = 0;
+        while (x < 5) {
+            drake.putThing();
+            drake.move();
+            x++;
+        }
+        drake.turnRight();
+        drake.move();
+        drake.turnRight();
+        drake.move();
+        while (x < 10) {
+            drake.putThing();
+            drake.move();
+            x++;
+        }
     }
 }
