2f181c2190e8281bdbd832934c8d96b01ede64c5
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-02-27 10:41:33 -0500

    Assignment3 Q1, Q2 and Q3 complete

--- src/Assignment3/A3Q3.java
+++ src/Assignment3/A3Q3.java
@@ -25,12 +25,16 @@ public class A3Q3 {
         //put a robot in my city and give it 20 things
         RobotSE drake = new RobotSE(ovo, 1, 1, Direction.EAST, 20);
         drake.setColor(Color.red); //make drake red
+        //set x to 0
         int x = 0;
+        /*drake will put down five things then move down to the next row and put another five
+         until he runs out of things and makes a 5 by 4 rectangle of things.*/
         while (x < 5) {
             drake.putThing();
             drake.move();
             x++;
         }
+
         drake.turnRight();
         drake.move();
         drake.turnRight();
@@ -40,5 +44,29 @@ public class A3Q3 {
             drake.move();
             x++;
         }
+        drake.turnLeft();
+        drake.move();
+        drake.turnLeft();
+        drake.move();
+
+        while (x < 15) {
+            drake.putThing();
+            drake.move();
+            x++;
+        }
+
+        drake.turnRight();
+        drake.move();
+        drake.turnRight();
+        drake.move();
+        while (x < 20) {
+            drake.putThing();
+            drake.move();
+            x++;
+        }
+        drake.turnLeft();
+        drake.move();
+        drake.turnLeft();
+        drake.move();
     }
 }
