eaeda25bc285d508fb1a043a1c6d69db301e39e7
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-02-28 10:38:23 -0500

    Assignment3 Q1, Q2 and Q3 and Q4 complete

--- src/Assignment3/A3Q4.java
+++ src/Assignment3/A3Q4.java
@@ -25,7 +25,8 @@ public class A3Q4 {
         City ovo = new City();
         //put a robot in my city
         RobotSE drake = new RobotSE(ovo, 3, 3, Direction.SOUTH);
-        drake.setColor(Color.red); //make drake red
+        //make drake red
+        drake.setColor(Color.red);
         //top left square
         new Wall(ovo, 1, 1, Direction.NORTH);
         new Wall(ovo, 1, 2, Direction.NORTH);
@@ -53,7 +54,7 @@ public class A3Q4 {
         new Wall(ovo, 5, 2, Direction.EAST);
         new Wall(ovo, 5, 2, Direction.SOUTH);
         new Wall(ovo, 5, 1, Direction.SOUTH);
-//bottom right square
+        //bottom right square
         new Wall(ovo, 4, 4, Direction.NORTH);
         new Wall(ovo, 4, 5, Direction.NORTH);
         new Wall(ovo, 4, 4, Direction.WEST);
@@ -62,10 +63,8 @@ public class A3Q4 {
         new Wall(ovo, 5, 5, Direction.EAST);
         new Wall(ovo, 5, 5, Direction.SOUTH);
         new Wall(ovo, 5, 4, Direction.SOUTH);
-        /*drake. will move around the first square then continue forward and repeat that 
-         4 times to go around all 4 of the squares. 
- 
-         */
+        /*drake will move around the first square then continue forward and repeat that 
+         4 times to go around all 4 of the squares.*/
         for (int y = 0; y < 4; y++) {
             for (int x = 0; x < 3; x++) {
                 drake.move(3);
