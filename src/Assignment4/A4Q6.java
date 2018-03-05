69f037a864de590dd8918e7653edf2ad91e692ab
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-03-05 11:18:56 -0500

    Assignment4  Q1,Q2,Q3, Q4,Q5 completed

---
+++ src/Assignment4/A4Q6.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mathg8825
+ */
+public class A4Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      
+      
+    }
+}
