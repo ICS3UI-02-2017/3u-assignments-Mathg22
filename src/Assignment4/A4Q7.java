0cf137c5f2bde1f3037891cf6d1377728bf526c8
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-03-07 11:17:45 -0500

    Assignment4  Q1,Q2,Q3, Q4,Q5,Q6 completed

--- src/Assignment4/A4Q7.java
+++ src/Assignment4/A4Q7.java
@@ -19,18 +19,47 @@ public class A4Q7 {
         // TODO code application logic here
          Scanner input = new Scanner(System.in);
          int square = 1;
-         while(square<100){
+         int number = 0;
+         while(square!=100&&number!=5){
          System.out.println("Enter sum of the dice:");
-         int sum = input.nextInt();
-         square = sum + square;
-         System.out.println("You are now on square " + square);
+        int sum = input.nextInt();
+         if(sum>12||sum<0){
+             number = 5;
+         }
+
+         
          if(square==54){
              square = 19;
          }
          
+         if(square==90){
+         square = 48;    
+         
          }
+         if(square==99){
+             square = 77;
+         }
+         if(square==9){
+             square = 34;
+         }
+         if(square==40){
+             square = 64;
+         }
+         if(square==67){
+             square = 86;
+         }
+                  square = sum + square;
+         System.out.println("You are now on square " + square);
          
-         System.out.println("Congratulations! You are now on square 100 and you win!");
+         }
+         if(number==5){
+             System.out.println("You Quit!");
+         }
+         if(square==100){
+            System.out.println("Congratulations! You win!");  
+         }
+         }
+        
          
-    }
+    
 }
