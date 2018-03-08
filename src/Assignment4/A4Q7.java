99d2519c3b973acdd8b6de8288aee72739fe30ab
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-03-08 10:47:57 -0500

    Assignment4  Q1,Q2,Q3, Q4,Q5,Q6,Q7 completed

--- src/Assignment4/A4Q7.java
+++ src/Assignment4/A4Q7.java
@@ -16,50 +16,59 @@ public class A4Q7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-         Scanner input = new Scanner(System.in);
-         int square = 1;
-         int number = 0;
-         while(square!=100&&number!=5){
-         System.out.println("Enter sum of the dice:");
-        int sum = input.nextInt();
-         if(sum>12||sum<0){
-             number = 5;
-         }
 
-         
-         if(square==54){
-             square = 19;
-         }
-         
-         if(square==90){
-         square = 48;    
-         
-         }
-         if(square==99){
-             square = 77;
-         }
-         if(square==9){
-             square = 34;
-         }
-         if(square==40){
-             square = 64;
-         }
-         if(square==67){
-             square = 86;
-         }
-                  square = sum + square;
-         System.out.println("You are now on square " + square);
-         
-         }
-         if(number==5){
-             System.out.println("You Quit!");
-         }
-         if(square==100){
-            System.out.println("Congratulations! You win!");  
-         }
-         }
-        
-         
-    
+        Scanner input = new Scanner(System.in);
+//the player starts at square 1
+        int square = 1;
+//the player has not won yet and must keep playing until they reach 100 and win
+        while (square != 100) {
+            System.out.println("Enter sum of the dice:");
+
+            int sum = input.nextInt();
+            //if the player has "rolled" an imppossible number the program will terminate  
+            if (sum > 12 || sum < 1) {
+
+                System.out.println("You Quit!");
+                System.exit(sum);
+            }
+//the new square the player is on 
+            square = sum + square;
+            //if the player lands on the head of a snake and must go down it's tail
+            if (square == 54) {
+                square = 19;
+            }
+
+            if (square == 90) {
+                square = 48;
+
+            }
+            if (square == 99) {
+                square = 77;
+            }
+            //if the player lands at the base of a ladder and must climb it
+            if (square == 9) {
+                square = 34;
+            }
+            if (square == 40) {
+                square = 64;
+            }
+            if (square == 67) {
+                square = 86;
+            }
+            /*if the player has a number that is impossible because it is higher than 
+             * 100 then their square will be reset and they will be told why.
+             */
+            if (square > 100) {
+                square = square - sum;
+                System.out.println("You rolled too high! Try again.");
+            }
+            System.out.println("You are now on square " + square);
+
+
+        }
+
+//the player wins and the game is over.
+        System.out.println("Congratulations! You win!");
+
+    }
 }
