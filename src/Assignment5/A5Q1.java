4891d9c0be69aa81db3cb93e309276af02099ce2
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-03-09 11:15:38 -0500

    Assignment5  started

---
+++ src/Assignment5/A5Q1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mathg8825
+ */
+public class A5Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the sentence you wish to translate into Ubbi Dubbi here:");
+        String sentence = input.nextLine();
+        
+        
+    }
+}
