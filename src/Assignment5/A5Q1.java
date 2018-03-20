e425b090cbbf7e7224f633859d7b6036382492d7
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-03-20 11:18:20 -0400

    Assignment5  started

--- src/Assignment5/A5Q1.java
+++ src/Assignment5/A5Q1.java
@@ -20,8 +20,33 @@ public class A5Q1 {
         Scanner input = new Scanner(System.in);
         
         System.out.println("Enter the sentence you wish to translate into Ubbi Dubbi here:");
-        String sentence = input.nextLine();
-      //  if(sentence.contains('A'+'E'+'I'+'O'+'U'));
+        String origword = input.nextLine();
         
+        origword= origword.toLowerCase();
+        
+     //   if(origword.contains("e")||origword.contains("a")||origword.contains("i")||
+        //        origword.contains("o")||origword.contains("u"))
+            {
+      int length = origword.length();
+                //use a for loop to go through the characters
+                for(int i = 0; i< length; i++){
+                    //look at the character at position , is it a vowel?
+                    if(origword.charAt(i) == 'a'||
+                    origword.charAt(i) == 'e'||
+                    origword.charAt(i) == 'i'||
+                    origword.charAt(i) == 'o'||
+                    origword.charAt(i) == 'u'){
+                        //i is the position of the vowel
+                        String start = origword.substring(0,i);
+                        String end = origword.substring(i);
+                        String transword = start + "ub"+end;
+               
+                        System.out.println(origword+" in Ubbi Dubbi is "+transword);
+                   }
+                    
+                    }
+                    
+
+    }
     }
 }
