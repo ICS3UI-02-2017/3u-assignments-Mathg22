f899e2a81e6863579aae1dbfb8c257263cc9fe3b
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-04-23 11:18:51 -0400

    Assignment6 Q1 Q2 done

--- src/Assignment6/A6Q5.java
+++ src/Assignment6/A6Q5.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  *
  * @author mathg8825
  */
-public class A6Q4 {
+public class A6Q5 {
 
     /**
      * @param args the command line arguments
@@ -18,19 +18,21 @@ public class A6Q4 {
     public static void main(String[] args) {
         //set input as scanner to read info
         Scanner input = new Scanner(System.in);
-        //created array to hold 10 marks
-        int[] marks = new int[10];
+        System.out.println("How many marks do you wish to enter?");
+        int v = input.nextInt();
+        //created array to hold the desired amount of marks
+        int[] marks = new int[v];
         //ask the user for the marks
-        System.out.println("Please enter the ten marks:");
+        System.out.println("Please enter the marks:");
         //use a for loop to store the marks in the array
         for (int i = 0; i < marks.length; i++) {
             marks[i] = input.nextInt();
         }
         //varaible created that is one less than array length to be used to limit for loops
         int c = marks.length - 1;
-        /*use a for loop to analyze each mark in the array by starting at the start of the 
-         array and going up by one to the end and switching the values where 
-         need to make the array in ascending order*/
+            /*use a for loop to analyze each mark in the array by starting at the start of the 
+            array and going up by one to the end and switching the values where 
+            need to make the array in ascending order*/
         for (int i = 0; i < c; i++) {
             //varaible to store the mark at the position i in the array
             int AA = marks[i];
@@ -44,8 +46,8 @@ public class A6Q4 {
                 marks[(i + 1)] = AA;
             }
             /*use a for loop to analyze each mark in the array by starting at the end of the 
-             array and going down by one to the start and switching the values where 
-             need to make the array in ascending order*/
+            array and going down by one to the start and switching the values where 
+            need to make the array in ascending order*/ 
             for (int d = c; d > 0; d--) {
                 //varaible to store the mark at the position d in the array
                 AA = marks[d];
@@ -59,10 +61,11 @@ public class A6Q4 {
                     marks[(d - 1)] = AA;
                 }
             }
-
+            
         }
         //tell the user the marks in ascending order
         System.out.println("The marks in ascending order is " + marks[0] + " " + marks[1] + " " + marks[2] + " " + marks[3] + " "
                 + marks[4] + " " + marks[5] + " " + marks[6] + " " + marks[7] + " " + marks[8] + " " + marks[9]);
     }
+    
 }
