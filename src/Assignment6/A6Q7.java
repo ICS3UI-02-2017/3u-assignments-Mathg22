744355b9effecb1de39b957e9f9f62697ff8608c
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-04-27 11:18:22 -0400

    Assignment6 Q1 Q2 Q3 Q4 Q5Q6 done

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -17,13 +17,45 @@ public class A6Q7 {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int[] n = new int[1000];
+            boolean[] numbers= new boolean[1000];
+            for(int i = 0; i<1000; i++){
+                numbers[i] = true;
+            }
+            
+      //   for( int i = 0; i<999; i++){
+       //      n[i] = n[i] +1 ;
+       //  }
         
-        for(int i =  2; i<1000; i++){
-            int[] numbers= new int[i];
-            int a = 0;
-            a = a++;
-            numbers[a] = i;
-        }
-       int p = 2;
+       int p = 0;
+       for( int i=2; i<999; i++){
+          int number = n[i];
+           if(number % 2 ==0 ){
+               numbers[i] = false; 
+           }
+           }
+           for( int i=3; i<1000; i++){
+          int number = n[i];
+           if(number % 3 ==0 ){
+               numbers[i] = false; 
+           
+           }
+           }
+           for( int i=5; i<1000; i++){
+          int number = n[i];
+           if(number % 5 ==0 ){
+               numbers[i] = false; 
+           
+           }
+           }
+           
+           for( int i=7; i<1000; i++){
+          int number = n[i];
+           if(number % 3 ==0 ){
+               numbers[7] = false; 
+           
+           }
+           }
     }
 }
+
