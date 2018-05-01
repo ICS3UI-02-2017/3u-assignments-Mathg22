1f2ebf19470a92e4e67ccd6171d4146589e05a07
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-05-01 11:18:34 -0400

    Assignment6 Q1 Q2 Q3 Q4 Q5Q6 Q7 done

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -17,54 +17,80 @@ public class A6Q7 {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        //array to store numerical values of 1-1000 in
         int[] n = new int[1000];
-            boolean[] numbers= new boolean[1000];
-            for(int i = 0; i<1000; i++){
-                numbers[i] = true;
-            }
-            numbers[0]= false;
-            numbers[1]=false;
-         for( int i = 0; i<999; i++){
-             n[i] = i ;
+        //array to mark which numbers are prime or not
+        boolean[] numbers = new boolean[1000];
+        //set whole array to true
+        for (int i = 0; i < 1000; i++) {
+            numbers[i] = true;
         }
-        
-       int p = 0;
-       for( int i=3; i<999; i++){
-          int number = n[(i+1)];
-           if(number % 2 ==0 ){
-               numbers[i] = false; 
-           }
-           }
-           for( int i=4; i<999; i++){
-          int number = n[(i+1)];
-           if(number % 3 ==0 ){
-               numbers[i] = false; 
-           
-           }
-           }
-           for( int i=6; i<999; i++){
-          int number = n[(i+1)];
-           if(number % 5 ==0 ){
-               numbers[i] = false; 
-           
-           }
-           }
-           
-           for( int i=8; i<999; i++){
-               
-          int number = n[(i+1)];
-           if(number % 7 ==0 ){
-               numbers[i] = false; 
-           
-           }
-           
-           }
-           System.out.println("The prime numbers are ");
-           for(int i = 2; i<1000; i++){
-               if(numbers[i]= true){
-                   System.out.println(""+n[i]);
-               }
-           }
+        //set 0 and 1 as false because they can not be prime numbers
+        numbers[0] = false;
+        numbers[1] = false;
+        //create the values of 1-1000
+        for (int i = 0; i < 1000; i++) {
+            n[i] = i;
+        }
+
+        int p = 0;
+        //starting at 3 because 2 is a prime number
+        //check all numbers between 3-1000 if they are a factor of 2. If they are have them equal false
+        for (int i = 3; i < 1000; i++) {
+            int number = n[i];
+            if (number % 2 == 0) {
+                numbers[i] = false;
+            }
+        }
+        //starting at 4 because 3 is a prime number
+        //check all numbers between 4-1000 if they are a factor of 3. If they are have them equal false
+        for (int i = 4; i < 1000; i++) {
+            int number = n[i];
+            if (number % 3 == 0) {
+                numbers[i] = false;
+
+            }
+        }
+        //starting at 6 because 5 is a prime number
+        //check all numbers between 6-1000 if they are a factor of 5. If they are have them equal false
+        for (int i = 6; i < 1000; i++) {
+            int number = n[i];
+            if (number % 5 == 0) {
+                numbers[i] = false;
+
+            }
+        }
+        //starting at 8 because 7 is a prime number
+        //check all numbers between 8-1000 if they are a factor of 7. If they are have them equal false
+        for (int i = 8; i < 1000; i++) {
+            int number = n[i];
+            if (number % 7 == 0) {
+                numbers[i] = false;
+
+            }
+
+/*using the for loop to use the new prime numbers to eliminate other factors of themselves 
+ which say they are prime numbers when really they are a factor of this new prime number. 
+ Starts from the bottom to ensure all other numbers with this factor are eliminated.*/
+            for (int b = 11; b < 1000; b++) {
+                if (numbers[b]) {
+
+                    for (i = (b + 1); i < 999; i++) {
+                        int numb = n[i];
+                        if (numb % b == 0) {
+                            numbers[i] = false;
+                        }
+                    }
+                }
+
+            }
+            //tell user prime numbers using a for loop that tells the user all the numbers with a true value
+            System.out.println("The prime numbers are: ");
+            for (i = 2; i < 1000; i++) {
+                if (numbers[i]) {
+                    System.out.println("" + n[i]);
+                }
+            }
+        }
     }
 }
-
