f7b506c3e66772cba175a7ac6ce188cc02cade8b
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-05-09 11:19:09 -0400

    Assignment7 started

--- src/Assignment7/A7Q5.java
+++ src/Assignment7/A7Q5.java
@@ -10,12 +10,27 @@ package Assignment7;
  * @author matha
  */
 public class A7Q5 {
+//method to generate a random number of asteriks between 5 and 1 
+
+    public static void chaotic(int a) {
+
+        for (int i = 0; i < a; i++) {
+            int random = (int) (Math.random() * (5 - 1 + 1)) + 1;
+            System.out.println("");
+            for (int b = 0; b < random; b++) {
+                System.out.print("*");
+            }
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+
+        int a = (int) (Math.random() * (100 - 1 + 1)) + 1;
+
+        chaotic(a);
     }
-    
 }
