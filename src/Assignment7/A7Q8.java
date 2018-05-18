621a25f39bacb41913fbd8295ef8b74b73a8355a
mathg8825 <mathg8825@HRH-IC0027331.wrdsb.ca>
2018-05-18 11:19:47 -0400

    Assignment8

--- src/Assignment7/A7Q8.java
+++ src/Assignment7/A7Q8.java
@@ -4,16 +4,30 @@
  */
 package Assignment7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mathg8825
  */
 public class A7Q8 {
 
+    public static int allDigitsOdd(int digit){
+        String number = String.valueOf(digit);
+        int length = number.length();
+        int lastint = length - 1;
+        String last = number.substring(length);
+        return lastint;
+        
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number you want to find the last digit of here:");
+        int digit = input.nextInt();
+        int a = allDigitsOdd(digit);
+        
     }
 }
